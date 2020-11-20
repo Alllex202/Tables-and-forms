28aecc1d458621e0bfc498e75363b82493e417e3
Alllex202 <Alllex.202@gmail.com>
2020-11-20 13:49:58 +0500

    server.js without parser

---
+++ addOrder.js
@@ -0,0 +1,29 @@
+module.exports = class Table {
+    // let _head = '';
+    // let _body = '';
+
+    constructor(titles) {
+        this.head = '';
+        this.body = '';
+        if (this.head === '') {
+            this.head = `<tr>${titles.map(title => `<th>${title}</th>`).join('')}</tr>`;
+        }
+    }
+
+    addOrder(data) {
+        this.body += `<tr>
+<td>${data.petType === 'cat' && 'Кот' || data.petType === 'dog' && 'Собака' || data.petType === 'tiger' && 'Тигр'}</td>
+<td>${data.gender === 'girl' && 'Ж' || data.gender === 'boy' && 'М' || 'Любой'}</td>
+<td>${/^#[0-9a-f]{6}$/i.test(data.eyeColor) && `<div class="block-color" style="background-color: ${data.eyeColor}"><div>`}</td>
+<td>${/^[0-9]{1,3}$/.test(data.tailLength) && `${data.tailLength} см`}</td>
+<td>${data.name}</td>
+<td>${/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/.test(data.dateOfBirth) && data.dateOfBirth.split('-').reverse().join('-')}</td>
+<td>${/^.+@.+\..+$/igm.test(data.email) && `<a href="mailto:${data.email}">${data.email}</a>`}</td>
+<td>${data.phone}</td>
+</tr>`;
+    }
+
+    get() {
+        return `<table class="orders"><thead>${this.head}</thead><tbody>${this.body}</tbody></table>`
+    }
+}
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+// import {Table} from "./addOrder";
+
 const express = require('express');
 const app = express();
 const port = 3000;
 const constructPageBody = require('./checkOrder');
+const Table = require('./addOrder.js');
 
 
 const pageHead =
@@ -37,60 +40,74 @@ app.listen(port, (err) => {
 app.use(express.static('static'));
 app.use(express.urlencoded({extended: true}));
 
+
+const table = new Table(['Животное', 'Пол', 'Цвет глаз', 'Длина хвоста', 'Заказчик', 'Дата рождения', 'E-mail', 'Телефон']);
+
 app.post('/pets/orders', (request, response) => {
     const reqBody = request.body;
     let pageBody = constructPageBody(reqBody);
-    let arr = pageBody.split('\n');
 
-    // Правка заголовков
-    arr = arr.map(el => {
-        if (el === '<th>Тип животного</th>') {
-            el = '<th>Животное</th>';
-        } else if (el === '<th>Имя хозяина</th>') {
-            el = '<th>Заказчик</th>';
-        } else if (el === '<th>email</th>') {
-            el = '<th>E-mail</th>';
-        }
-        return el;
-    });
+    if (/<table>|<\/table>/.test(pageBody)) {
+        table.addOrder(reqBody);
+        pageBody = `<h3>Ваш заказ добавлен!</h3>${table.get()}`;
+    }
 
-    // Правка тела
-    const tableBody = `<tr>${arr[arr.length - 1]
-        .split(/<tr>|<\/tr>|<\/table>/)
-        .filter(e => e)
-        .map(el => el.split(/<td>|<\/td>/).filter(e => e))
-        .map(el => el.map((el, ind) => {
-                if (ind === 0) {
-                    el = el === 'cat' && 'Кот'
-                        || el === 'dog' && 'Собака'
-                        || el === 'tiger' && 'Тигр';
-                } else if (ind === 1) {
-                    el = el === 'girl' && 'Ж'
-                        || el === 'boy' && 'М'
-                        || 'Любой';
-                } else if (ind === 2) {
-                    el = /^#[0-9a-f]{6}$/i.test(el) && `<div class="block-color" style="background-color: ${el}"><div>`;
-                } else if (ind === 3) {
-                    el = /^[0-9]{1,3}$/.test(el) && `${el} см`;
-                    // } else if (ind === 4) {
-                    //
-                } else if (ind === 5) {
-                    el = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/.test(el) && el.split('-').reverse().join('-');
-                } else if (ind === 6) {
-                    el = /^.+@.+\..+$/igm.test(el) && `<a href="mailto:${el}">${el}</a>`;
-                    // } else if (ind === 7) {
-                    //
-                }
-                return `<td>${el}</td>`;
-            }).join('')
-        ).join('</tr><tr>')}</tr></table>`;
-    arr.pop();
+    // Парсер
 
-    // Объединение результата
-    arr.push(tableBody)
-    pageBody = arr.join('');
+    // let arr = pageBody.split('\n');
+    //
+    // // Правка заголовков
+    // arr = arr.map(el => {
+    //     if (el === '<th>Тип животного</th>') {
+    //         el = '<th>Животное</th>';
+    //     } else if (el === '<th>Имя хозяина</th>') {
+    //         el = '<th>Заказчик</th>';
+    //     } else if (el === '<th>email</th>') {
+    //         el = '<th>E-mail</th>';
+    //     }
+    //     return el;
+    // });
+    //
+    // // Правка тела
+    // const tableBody = `<tr>${arr[arr.length - 1]
+    //     .split(/<tr>|<\/tr>|<\/table>/)
+    //     .filter(e => e)
+    //     .map(el => el.split(/<td>|<\/td>/).filter(e => e))
+    //     .map(el => el.map((el, ind) => {
+    //             if (ind === 0) {
+    //                 el = el === 'cat' && 'Кот'
+    //                     || el === 'dog' && 'Собака'
+    //                     || el === 'tiger' && 'Тигр';
+    //             } else if (ind === 1) {
+    //                 el = el === 'girl' && 'Ж'
+    //                     || el === 'boy' && 'М'
+    //                     || 'Любой';
+    //             } else if (ind === 2) {
+    //                 el = /^#[0-9a-f]{6}$/i.test(el) && `<div class="block-color" style="background-color: ${el}"><div>`;
+    //             } else if (ind === 3) {
+    //                 el = /^[0-9]{1,3}$/.test(el) && `${el} см`;
+    //                 // } else if (ind === 4) {
+    //                 //
+    //             } else if (ind === 5) {
+    //                 el = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/.test(el) && el.split('-').reverse().join('-');
+    //             } else if (ind === 6) {
+    //                 el = /^.+@.+\..+$/igm.test(el) && `<a href="mailto:${el}">${el}</a>`;
+    //                 // } else if (ind === 7) {
+    //                 //
+    //             }
+    //             return `<td>${el}</td>`;
+    //         }).join(''))
+    //     .join('</tr><tr>')}</tr></table>`;
+    // arr.pop();
+    //
+    // // Объединение результата
+    // arr.push(tableBody)
+    // pageBody = arr.join('');
 
-    // console.log(tableBody)
+    // console.log(reqBody)
+    // console.log(++table);
+
+    console.log([table, pageBody])
 
     response.send(`${pageHead}${pageBody}${pageFoot}`);
 });
